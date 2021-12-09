f41e923880d1a988b388e28b068c09c8e3460edc
kjkuhle <95831879+kjkuhle@users.noreply.github.com>
2021-12-08 18:38:03 -0600

    Add files via upload

---
+++ ass2.3.py
@@ -0,0 +1,6 @@
+# This first line is provided for you
+rate = input("Enter rate per hour:")
+hrs = input("Enter Hours:")
+grosspay = float(rate) * int(hrs)
+print('Pay:', grosspay)
+input('press any key to exit')
